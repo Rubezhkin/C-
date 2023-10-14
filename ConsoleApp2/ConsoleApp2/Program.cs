cf63490b0c61665bc4a29b25ed285e72d91a6e68
Pavel Nedobezhkin <96119105+Rubezhkin@users.noreply.github.com>
2023-10-14 20:05:09 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,76 @@
+﻿public class Stack<T>
+{
+    private Node<T> head;
+
+    public Node<T> top()
+    { 
+        return head; 
+    }
+    public Stack<T> tail()
+    {
+        Stack<T> St = new Stack<T>();
+        St.head = head.next;
+        return St;
+    }
+    public void Push(T value)
+    {
+        Node<T> newNode = new Node<T>();
+        newNode.Value = value;
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public T Pop()
+    {
+        if (head == null)
+            throw new InvalidOperationException("Стэк пуст");
+
+        T value = head.Value;
+        head = head.next;
+        return value;
+    }
+
+    public class Node<T>
+    {
+        public T Value;
+        public Node<T> next;
+    }
+}
+
+public class Tasc
+{
+    public static void Main(string[] args)
+    {
+        Stack<int> stack = new Stack<int>();
+        for(int i = 0; i < 5; i++) 
+            stack.Push(i);
+        int mode;
+        Console.WriteLine("1)верхнее значение\n2)хвост\n3)удалить\n4)добавить\n0)выйти");
+        mode = Convert.ToInt32(Console.ReadLine());
+        while (mode != 0)
+        {
+            switch (mode)
+            {
+                case 1:
+                    Console.WriteLine(stack.top().Value);
+                    break;
+                case 2:
+                    Stack<int> st = stack.tail();
+                    while (st.top() != null) Console.Write(st.Pop() + " ");
+                    Console.WriteLine("\n");
+                    break;
+                case 3:
+                    try
+                    { stack.Pop(); }
+                    catch (Exception ex)
+                    { Console.WriteLine(ex.Message); }
+                    break;
+                case 4:
+                    Console.WriteLine("Введите значение");
+                    stack.Push(Convert.ToInt32(Console.ReadLine()));
+                    break;
+            }
+            mode = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,45 @@
+﻿struct Domino
+{
+    public int n1, n2;
+};
+
+class laba3
+{
+    public static void Main(String[] args)
+    {
+        string str;
+        string[] str2;
+        Console.Write("Введите количество доминошек: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        Domino[] domino = new Domino[N];
+        for (int i = 0; i < N; i++)
+        {
+            Console.Write("Введите значения " + (i + 1) + " доминошки: ");
+            str = Console.ReadLine();
+            str2 = str.Split(' ');
+            domino[i].n1 = Convert.ToInt32(str2[0]);
+            domino[i].n2 = Convert.ToInt32(str2[1]);
+            if (domino[i].n1 > domino[i].n2)
+                (domino[i].n1, domino[i].n2) = (domino[i].n2, domino[i].n1);     
+        }
+
+        Console.WriteLine("Результат: " + func(domino));
+    }
+    public static int func(Domino[] domino)
+    {
+        Dictionary<Domino,int> dict = new Dictionary<Domino, int>();
+        foreach (Domino dom in domino)
+        {
+            if (!dict.ContainsKey(dom))
+                dict.Add(dom, 1);
+            else
+                dict[dom]++;
+        }
+        int res = 0;
+        foreach (var dom in dict)
+            if (dom.Value != 1)
+                res += dom.Value;
+        
+        return res;
+    }
+}
